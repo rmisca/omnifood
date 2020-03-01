d0ade4c452ca10605d4b41d11481b3207b133bb5
rmisca <rominamisca95@gmail.com>
2020-03-01 20:12:29 +0200

    Add mobile navigation icon, add animation.

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
           $('nav').removeClass('sticky');
       }
   }, {
-    offset: '60px'
+    offset: '60px;'
   }); 
 
 
@@ -21,6 +21,85 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
   });
 
+
+
+
+  /* Navigation scroll*/
+$(function(){
+    // Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+})
+/* Animations on scroll  */
+
+$('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+}, {
+    offset: '50%'
+});
+
+$('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+}, {
+    offset: '50%'
+});
+ 
+$('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+}, {
+    offset: '50%'
+});
+$('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+}, {
+    offset: '50%'
+});
+
+/* Mobile navigation */
+$('.js--nav-icon').click(function() {
+    var nav= $('.js--main-nav');
+    var icon = $('.js--nav-icon ion-icon');
+    nav.slideToggle(200);
+    if (icon.attr('name') === "menu-outline") {
+        icon.attr("name", "close-outline");
+    } else {
+        icon.attr("name", "menu-outline");
+    }
+
+});
 
 
 
